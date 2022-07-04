d1a1ade993ca62d65a2ea4ad286a1975b8ffcab5
Licon <1163760935@qq.com>
2022-07-04 23:09:38 +0800

    添加oauth2认证/授权服务

---
+++ spring-security-auth/src/test/java/com/licon/auth/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licon.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
