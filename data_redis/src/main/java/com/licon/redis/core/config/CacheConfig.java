8dc5cc36fdbe81c209d8dfca9ec9e0cabc4716a0
Licon <lvshaowei@cpic.com.cn>
2022-08-10 14:44:14 +0800

    缓存配置添加单机环境

--- data_redis/src/main/java/com/licon/redis/core/config/CacheConfig.java
+++ data_redis/src/main/java/com/licon/redis/core/config/CacheConfig.java
@@ -3,12 +3,10 @@ package com.licon.redis.core.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
-import org.redisson.config.SentinelServersConfig;
 import org.redisson.config.SingleServerConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
@@ -18,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -28,18 +27,31 @@ public class CacheConfig {
 
     private final RedisProperties redisProperties;
     private final Environment env;
+    private final static String COM_ENV="com";
+    private final static String HOM_ENV="hom";
 
     @Bean(destroyMethod = "shutdown")
     RedissonClient redisson() {
         Config config = new Config();
-        List<String> clusterNodes = new ArrayList<>();
-        for (int i = 0; i < redisProperties.getCluster().getNodes().size(); i++) {
-            clusterNodes.add("redis://" + redisProperties.getCluster().getNodes().get(i));
-        }
-        ClusterServersConfig clusterServersConfig = config.useClusterServers()
-                .addNodeAddress(clusterNodes.toArray(new String[0]));
-        if (redisProperties.getPassword() != null) {
-            clusterServersConfig.setPassword(redisProperties.getPassword());
+        String[] activeProfiles = env.getActiveProfiles();
+        if (Arrays.stream(activeProfiles).anyMatch(COM_ENV::equalsIgnoreCase)){
+            SingleServerConfig singleServerConfig = config.useSingleServer()
+                    .setClientName(redisProperties.getClientName())
+                    .setAddress("redis://"+redisProperties.getHost()+":"+redisProperties.getPort())
+                    .setDatabase(redisProperties.getDatabase());
+            if (redisProperties.getPassword() != null) {
+                singleServerConfig.setPassword(redisProperties.getPassword());
+            }
+        }else if(Arrays.stream(activeProfiles).anyMatch(HOM_ENV::equalsIgnoreCase)) {
+            List<String> clusterNodes = new ArrayList<>();
+            for (int i = 0; i < redisProperties.getCluster().getNodes().size(); i++) {
+                clusterNodes.add("redis://" + redisProperties.getCluster().getNodes().get(i));
+            }
+            ClusterServersConfig clusterServersConfig = config.useClusterServers()
+                    .addNodeAddress(clusterNodes.toArray(new String[0]));
+            if (redisProperties.getPassword() != null) {
+                clusterServersConfig.setPassword(redisProperties.getPassword());
+            }
         }
         return Redisson.create(config);
     }
